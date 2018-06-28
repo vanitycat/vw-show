017a17772287f94a3b050bb244855c7c2bf1d342
Alan McCappin <alan_mccappin@yahoo.co.uk>
2018-06-28 23:45:40 +0100

    finished sticky header

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,7 +1,9 @@
 import $ from 'Jquery';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 var mobileMenu = new MobileMenu;
+var stickyHeader = new StickyHeader;
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
