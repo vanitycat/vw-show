7ab6d238d24ef300f86ea8b4ebab6289d571d0b3
Alan McCappin <alan_mccappin@yahoo.co.uk>
2018-07-08 20:34:58 +0100

    finished hero section

--- app/assets/scripts/app.js
+++ app/assets/scripts/app.js
@@ -1,6 +1,7 @@
-import $ from 'Jquery';
+
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
 import Modal from './modules/Modal';
 
